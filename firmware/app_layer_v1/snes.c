3bb0cb27fcb4800adcfb8910e3c8b59d3231b8ac
Bruno Gouveia <bgouveia@gmail.com>
2012-01-12 01:36:25 +0000

    delay tests (snes and blink)

--- firmware/app_layer_v1/snes.c
+++ firmware/app_layer_v1/snes.c
@@ -2,9 +2,16 @@
 #include "snes.h"
 #include "protocol.h"
 #include "protocol_defs.h"
+#include "timer.h"
+#include "pins.h"
+
+#define FCY 32000000ULL
+#include <libpic30.h>
 
 static int SNESEnabled = 0;
-
+static int SNESlatchpin = 0;
+static int SNESdatapin = 0;
+static int SNESclockpin = 0;
 
 void SNESConfig(int data_pin,int clock_pin, int latch_pin){
 
@@ -14,16 +21,68 @@ void SNESConfig(int data_pin,int clock_pin, int latch_pin){
 
     // LATCH is Active HIGH
     SetPinDigitalOut(latch_pin,0,1);
-
+    
     // data as input  pull up
     SetPinDigitalIn(data_pin,1);
 
+    SNESdatapin=data_pin;
+    SNESlatchpin=latch_pin;
+    SNESclockpin=clock_pin;
     SNESEnabled=1;
 }
 
 void SNESTasks(){
     if (SNESEnabled){
-        //TODO: check shift register and send report
+
+	int i;
+	unsigned char tmp=0;
+
+        //SNES_LATCH_HIGH();
+        //SetDigitalOutLevel?
+        PinSetLat(SNESlatchpin,1);
+        __delay_us(12);
+	//_delay_us(12);
+	//SNES_LATCH_LOW();
+
+	/*for (i=0; i<8; i++)
+	{
+		_delay_us(6);
+		SNES_CLOCK_LOW();
+
+		tmp <<= 1;
+		if (!SNES_GET_DATA()) { tmp |= 1; }
+
+		_delay_us(6);
+
+		SNES_CLOCK_HIGH();
+	}
+	last_read_controller_bytes[0] = tmp;
+	for (i=0; i<8; i++)
+	{
+		_delay_us(6);
+
+		SNES_CLOCK_LOW();
+
+		// notice that this is different from above. We
+		// want the bits to be in reverse-order
+		tmp >>= 1;
+		if (!SNES_GET_DATA()) { tmp |= 0x80; }
+
+		_delay_us(6);
+		SNES_CLOCK_HIGH();
+	}
+	last_read_controller_bytes[1] = tmp;
+
+}
+
+static char snesChanged(char id)
+{
+	static int first = 1;
+	if (first) { first = 0;  return 1; }
+
+	return memcmp(last_read_controller_bytes,
+					last_reported_controller_bytes, GAMEPAD_BYTES);
+}*/
     }
 }
 
--- firmware/blink/main.c
+++ firmware/blink/main.c
@@ -2,6 +2,9 @@
 #define GetPeripheralClock()        (GetSystemClock())
 #define GetInstructionClock()       (GetSystemClock() / 2)
 
+#define FCY 32000000ULL
+#include <libpic30.h>
+
 #include "Compiler.h"
 #include "timer.h"
 
@@ -9,9 +12,18 @@ int main() {
   LATFbits.LATF3 = 0;
   while (1) {
     TRISFbits.TRISF3 = 0;
-    DelayMs(500);
+    int i;
+    for(i=0;i<5000;i++){
+        __delay_us(100);
+    }
+    //DelayMs(500);
     TRISFbits.TRISF3 = 1;
-    DelayMs(500);
+
+    for(i=0;i<5000;i++){
+        __delay_us(100);
+    }
+    
+    //DelayMs(500);
   }
   return 0;
 }
